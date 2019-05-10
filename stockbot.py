4f0e5fa066ff605b0a515765d9cfe4fe0734f9c8
nandvard <30357401+nandvard@users.noreply.github.com>
2019-05-10 13:05:11 -0400

    Add files via upload

---
+++ stockbot.py
@@ -0,0 +1,38 @@
+import praw             #Reddit API
+import pymysql          #AWS mySQL API
+import rdsconfig        
+import re
+import datetime
+import pandas_datareader as pdr #Stock API
+
+
+def lambda_handler(event,context):
+        date = datetime.datetime(2019, 5, 9)
+        datestr = date.strftime('%Y-%m-%d')
+        stock = pdr.data.DataReader('COF', 'iex', date, date)   #Closing price on 5/9/2019
+        price = stock.loc[datestr]['close']
+
+        conn = pymysql.connect(rdsconfig.host, rdsconfig.user, rdsconfig.passwd, rdsconfig.db)  
+        cur = conn.cursor()
+
+        r = praw.Reddit('bot')
+
+        for post in r.subreddit('bottest00').new(limit=10):          #Get new posts
+            if re.search("stock", post.title, re.IGNORECASE):   #Search keyword 'stock'
+                pid = post.id
+
+                sql1 = "select 1 from posts where id = %s"      #Avoid repeats
+                cur.execute(sql1,pid)
+
+                if cur.rowcount == 0:
+                    print (pid)
+                    post.reply(datestr+" COF close $"+str(price))       #Comment stock price
+                    post.upvote()
+
+                    sql2 = "insert into posts(id) values(%s)"           #Track post
+                    cur.execute(sql2,pid)
+                    conn.commit()
+
+        conn.close()
+
+#lambda_handler(0,0)
